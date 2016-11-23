99e08a4701910c5600229bdd244cdd7f2c01cffb
Attesa <Attesa@DESKTOP-FGCVJT7>
2016-11-23 16:39:05 +0300

    🙏

--- 6-Registry/Form1.Designer.cs
+++ 6-Registry/Form1.Designer.cs
@@ -28,12 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.filesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(554, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // filesToolStripMenuItem
+            // 
+            this.filesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.filesToolStripMenuItem.Name = "filesToolStripMenuItem";
+            this.filesToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.filesToolStripMenuItem.Text = "Files";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.newToolStripMenuItem.Text = "Open...";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.openToolStripMenuItem.Text = "Save";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(93, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(445, 211);
+            this.textBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "10 → 2",
+            "10 → 16",
+            "2  → 10",
+            "2 → 16",
+            "16 → 2",
+            "16  → 2"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(75, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(554, 255);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem filesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- 6-Registry/Form1.cs
+++ 6-Registry/Form1.cs
@@ -15,6 +15,98 @@ namespace _6_Registry
         public Form1()
         {
             InitializeComponent();
+            //this.comboBox1.SelectedItem = this.comboBox1.Items[0];
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private int convert_2_to_10(int n)
+        {
+            return 0;
+        }
+
+        private int convert_2_to_16(int n)
+        {
+            return 0;
+        }
+
+        private int convert_16_to_10(int n)
+        {
+            return 0;
+        }
+
+        private int convert_16_to_2(int n)
+        {
+            return 0;
+        }
+
+        private int convert_10_to_2(int n)
+        {
+            return 0;
+        }
+
+        private int convert_10_to_16(int n)
+        {
+            return 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string selected_text = this.textBox1.SelectedText;
+            decimal d;
+            String result = "";
+            //String result = Convert.ToString(Convert.ToInt32(selected_text, f), t);
+
+            switch (this.comboBox1.SelectedIndex)
+            {
+                //10 → 2
+                case 0:
+                    if (decimal.TryParse(selected_text, out d))
+                    {
+                        result = Convert.ToString(Convert.ToInt32(selected_text, 10), 2);
+                    }
+                    break;
+
+                //10 → 16
+                case 1:
+                    if (true)
+                    {
+                        result = Convert.ToString(Convert.ToInt32(selected_text, 10), 16);
+                    }
+                    break;
+
+                //2 → 10
+                case 2:
+                    if (true)
+                    {
+                        result = Convert.ToString(Convert.ToInt32(selected_text, 2), 10);
+                    }
+                    break;
+
+                //2 → 16
+                case 3:
+                    if (true)
+                    {
+                        result = Convert.ToString(Convert.ToInt32(selected_text, 2), 16);
+                    }
+                    break;
+
+                //16 → 2
+                case 4:
+                    if (true)
+                    {
+                        result = Convert.ToString(Convert.ToInt32(selected_text, 16), 2);
+                    }
+                    break;
+
+                //16 → 10
+                case 5:
+                    if (true)
+                    {
+                        result = Convert.ToString(Convert.ToInt32(selected_text, 16), 2);
+                    }
+                    break;
+
+            }
         }
     }
 }
